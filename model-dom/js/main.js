5c437594f29b9490681fb3f1a9c8f7a5044541b8
Anastasiia Holovachova <a.holovachova@gmail.com>
2018-12-02 14:59:02 +0100

    warsztat

---
+++ model-dom/js/main.js
@@ -0,0 +1,11 @@
+let buttonClick = document.getElementById("btn");
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+
+const ustawTlo = () => {
+   p1.style.backgroundColor = "red";
+   p2.style.backgroundColor = "yellow";
+}
+// element.addEventListener("btn", (e) => {
+
+// });
